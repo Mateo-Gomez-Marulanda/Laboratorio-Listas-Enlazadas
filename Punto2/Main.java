8eceb64ee648b340ea7d9e2850f2116756fbfddc
mateo <mateo.gomezm@uqvirtual.edu.co>
2026-03-27 07:33:02 -0500

    inicializando repositorio laboratorio listas enlazadas y listas dobles

---
+++ Punto2/Main.java
@@ -0,0 +1,20 @@
+package co.edu.uniquindio.poo.Unidad2.Laboratorio.Punto2;
+
+
+
+/*
+Escenario 2. Historial de navegación de un navegador web
+Un navegador web necesita almacenar las páginas visitadas por un usuario.
+El sistema debe permitir visitar una nueva página, regresar a la página anterior, 
+avanzar nuevamente si ya había retrocedido, 
+eliminar las páginas futuras cuando el usuario abre una nueva página después de retroceder, 
+y mostrar la página actual. El sistema necesita moverse en ambos sentidos dentro del historial.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+
